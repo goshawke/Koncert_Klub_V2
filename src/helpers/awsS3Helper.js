503943f24e61b8f18a709d7085967663f2ba2fa3
goshawke <goshawke@hushmail.com>
2024-11-15 17:50:05 -0500

    fixes S3 issue

--- src/helpers/awsS3Helper.js
+++ src/helpers/awsS3Helper.js
@@ -5,7 +5,7 @@ const getPublicMediaUrls = async (prefix) => {
   try {
     const response = await fetch(`https://${bucketName}.s3.${region}.amazonaws.com/media-urls.json`);
     let urls = await response.json();
-    urls = urls.filter(url => url.includes(`${prefix}/I`)); // Adjusted prefix filtering
+    urls = urls.filter(url => url.includes(`${prefix}/`)); // Adjusted prefix filtering
     urls = urls.filter(url => !url.includes(`DS_Store`)); // Filter out unwanted files
 
     return urls;
